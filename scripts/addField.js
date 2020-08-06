864944a77473601484cca2c80b6d76a4749d3115
RodrigoGrz <rodrigo.gregorio1@hotmail.com>
2020-08-05 21:49:56 -0300

    Atualização

---
+++ scripts/addField.js
@@ -0,0 +1,26 @@
+// Procurar o botão
+document.querySelector("#add-time")
+// Quando clicar no botão
+.addEventListener('click', cloneField)
+
+
+// Executar uma ação
+function cloneField() {
+    console.log("Cheguei aqui")
+    // Duplicar os campos. Que campo?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    // pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // para cada campo, limpar
+    fields.forEach(function(field) {
+        // pegar o field do momento e limpa ele
+        field.value = ""
+    })
+    
+    // Colocar na pagina: onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
+    
