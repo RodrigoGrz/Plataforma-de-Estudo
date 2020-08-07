6d9954016a15d6e8b642af280912c63f327049a0
RodrigoGrz <rodrigo.gregorio1@hotmail.com>
2020-08-07 17:48:37 -0300

    Completo

---
+++ src/database/fake_data.js
@@ -0,0 +1,17 @@
+const proffys = [
+    { name: "Rodrigo Grzegorczyk", 
+    avatar: "https://avatars2.githubusercontent.com/u/68669322?s=460&u=d9727ffd5a33f09e2e884a6875d9a8d7ec266f57&v=4",
+    whatsapp: "899765464",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    coast: "20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220] },
+     { name: "Daniele Evangelista ", avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", whatsapp: "8999876535",
+     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+     subject: "Química", coast: "20", weekday: [1], time_from: [720], time_to: [1220] },
+     { name: "Diego Fernandes", avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", whatsapp: "8999876535",
+     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+     subject: "Química", coast: "20", weekday: [0], time_from: [720], time_to: [1220] }
+    ]
